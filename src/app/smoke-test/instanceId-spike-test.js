7cf21826b422177703aa1119949f2001197d9971
JogoShugh <jsgough>
2015-02-27 07:44:18 -0500

    Streamlined the test using tranform functions to automatically parse to JSON, and reduce code clutter.
    S-51880

--- src/app/smoke-test/instanceId-spike-test.js
+++ src/app/smoke-test/instanceId-spike-test.js
@@ -14,100 +14,87 @@ function href(path) {
   return 'http://localhost:6565/api' + path;
 }
 
-var instance1 = {
-  instanceId: '2ef2b90b-52ae-4a2a-a606-09cb0c4d33c5',
-  apiKey: undefined,
-  digestCreateUri: undefined,
-  digest: {
-    description: 'First instance digest'
-  },  
-  inbox1: {
-    name: 'CommitStream.Web',
-    family: 'GitHub'
-  },
-  inboxCommits: {
-    "ref": "refs/heads/master",
-    "commits": [{
-      "id": "b42c285e1506edac965g92573a2121700fc92f8b",
-      "distinct": true,
-      "message": "S-11111 Updated Happy Path Validations!",
-      "timestamp": "2014-10-03T15:57:14-03:00",
-      "url": "https://github.com/kunzimariano/CommitService.DemoRepo/commit/b42c285e1506edac965g92573a2121700fc92f8b",
-      "author": {
-        "name": "shawnmarie",
-        "email": "shawn.abbott@versionone.com",
-        "username": "shawnmarie"
-      },
-      "committer": {
-        "name": "shawnmarie",
-        "email": "shawn.abbott@versionone.com",
-        "username": "shawnmarie"
-      },
-      "added": [],
-      "removed": [],
-      "modified": ["README.md"]
-    }],
-    "repository": {
-      "id": 23355501,
-      "name": "CommitService.DemoRepo"
-    }
-  }
-};
-
-var instance2 = {
-  instanceId: '453fcccd-3717-411d-8f87-3421b9effd79',
-  apiKey: undefined,
-  digestCreateUri: undefined,
-  digest: {
-    description: 'Second instance digest'
-  },
-  inbox1: {
-    name: 'psake-tools',
-    family: 'GitHub'
-  },
-  inboxCommits: {
-    "ref": "refs/heads/master",
-    "commits": [{
-      "id": "b42c285e1506edac965g92573a2121700fc92f8b",
-      "distinct": true,
-      "message": "S-11111 Hey all this stuff broke today, what's wrong?",
-      "timestamp": "2014-10-03T15:57:14-03:00",
-      "url": "https://github.com/kunzimariano/CommitService.DemoRepo/commit/b42c285e1506edac965g92573a2121700fc92f8b",
-      "author": {
-        "name": "marieshawn",
-        "email": "abbott.shawn@versionone.com",
-        "username": "shawnmarie"
-      },
-      "committer": {
-        "name": "marieshawn",
-        "email": "abbott.shawn@versionone.com",
-        "username": "marieshawn"
-      },
-      "added": [],
-      "removed": [],
-      "modified": ["README.md"]
-    }],
-    "repository": {
-      "id": 23355501,
-      "name": "CommitService.DemoRepo"
-    }
-  }
-};
-
-function show() {
-  console.log("OK...");
-}
-
-function post1(path, data) {
-  return {
-    uri: href(path),
-    method: 'POST',
-    headers: {
-        "Content-Type": "application/json"
+var instances = [
+  {
+    instanceId: '2ef2b90b-52ae-4a2a-a606-09cb0c4d33c5',
+    apiKey: undefined,
+    digestCreateUri: undefined,
+    digest: {
+      description: 'First instance digest'
+    },  
+    inbox1: {
+      name: 'CommitStream.Web',
+      family: 'GitHub'
     },
-    body: JSON.stringify(data)
-  };
-}
+    inboxCommits: {
+      "ref": "refs/heads/master",
+      "commits": [{
+        "id": "b42c285e1506edac965g92573a2121700fc92f8b",
+        "distinct": true,
+        "message": "S-11111 Updated Happy Path Validations!",
+        "timestamp": "2014-10-03T15:57:14-03:00",
+        "url": "https://github.com/kunzimariano/CommitService.DemoRepo/commit/b42c285e1506edac965g92573a2121700fc92f8b",
+        "author": {
+          "name": "shawnmarie",
+          "email": "shawn.abbott@versionone.com",
+          "username": "shawnmarie"
+        },
+        "committer": {
+          "name": "shawnmarie",
+          "email": "shawn.abbott@versionone.com",
+          "username": "shawnmarie"
+        },
+        "added": [],
+        "removed": [],
+        "modified": ["README.md"]
+      }],
+      "repository": {
+        "id": 23355501,
+        "name": "CommitService.DemoRepo"
+      }
+    }
+  },
+  // second instance:
+  {
+    instanceId: '453fcccd-3717-411d-8f87-3421b9effd79',
+    apiKey: undefined,
+    digestCreateUri: undefined,
+    digest: {
+      description: 'Second instance digest'
+    },
+    inbox1: {
+      name: 'psake-tools',
+      family: 'GitHub'
+    },
+    inboxCommits: {
+      "ref": "refs/heads/master",
+      "commits": [{
+        "id": "b42c285e1506edac965g92573a2121700fc92f8b",
+        "distinct": true,
+        "message": "S-11111 Hey all this stuff broke today, what's wrong?",
+        "timestamp": "2014-10-03T15:57:14-03:00",
+        "url": "https://github.com/kunzimariano/CommitService.DemoRepo/commit/b42c285e1506edac965g92573a2121700fc92f8b",
+        "author": {
+          "name": "marieshawn",
+          "email": "abbott.shawn@versionone.com",
+          "username": "shawnmarie"
+        },
+        "committer": {
+          "name": "marieshawn",
+          "email": "abbott.shawn@versionone.com",
+          "username": "marieshawn"
+        },
+        "added": [],
+        "removed": [],
+        "modified": ["README.md"]
+      }],
+      "repository": {
+        "id": 23355501,
+        "name": "CommitService.DemoRepo"
+      }
+    }
+  }
+];
 
 function post(uri, data, extraHeaders) {
   var headers = {
@@ -120,14 +107,20 @@ function post(uri, data, extraHeaders) {
     uri: uri,
     method: 'POST',
     headers: headers,
+    transform: function(body) { return JSON.parse(body); },
     body: JSON.stringify(data)
   };
+}
+
+function post1(path, data) {
+  return post(href(path), data);
 }
 
 function get(uri) {
   return {
     uri: href(uri),
     method: 'GET',
+    transform: function(body) { return JSON.parse(body); },
     headers: {
         "Content-Type": "application/json"
     }
@@ -138,7 +131,9 @@ function getLink(obj, relName) {
   return obj._links[relName].href;
 }
 
-describe('/api/instances', function() {
+var sep = '---------------';
+
+describe('/api/instances (and more)', function() {
   function instanceTest(instance, done) {
     rp(post1(
       '/instances' + apiKeyForAddingInstance,
@@ -146,47 +141,48 @@ describe('/api/instances', function() {
         instanceId: instance.instanceId
       }
     ))
-    .then(function(instanceBody) {
-      var instanceCreated = JSON.parse(instanceBody);
-      instance.digestCreateUri = getLink(instanceCreated, 'digest-create') + '?apiKey=' + instanceCreated.apiKey;
-      instance.apiKey = instanceCreated.apiKey;
+    .then(function(instanceCreated) {
+      instance.apiKey = instanceCreated.apiKey;      
+      instance.digestCreateUri = getLink(instanceCreated, 'digest-create') + '?apiKey=' + instance.apiKey;
       console.log('Created instance: ' + instanceCreated.instanceId);
       console.log('digest-create: ' + getLink(instanceCreated, 'digest-create'));
+      console.log(sep);      
     })    
     .then(function() {      
       return rp(post(instance.digestCreateUri, instance.digest));
     })
-    .then(function(digestBody) {
-      var digest = JSON.parse(digestBody);
-      var inboxCreateUri = digest._links['inbox-create'].href + '?apiKey=' + instance.apiKey;
+    .then(function(digest) {
+      var inboxCreateUri = getLink(digest, 'inbox-create') + '?apiKey=' + instance.apiKey;
       console.log('Created digest: ' + digest.digestId);
-      console.log('inbox-create: ' + digest._links['inbox-create'].href);
+      console.log('inbox-create: ' + getLink(digest, 'inbox-create'));
+      console.log(sep);
       return rp(post(inboxCreateUri, {
         name: 'CommitStream.Web',
         family: 'GitHub'
       }));
     })
-    .then(function(inboxBody) {
-      var inbox = JSON.parse(inboxBody);
-      var inboxPostCommitsUri = inbox._links['add-commit'].href + '?apiKey=' + instance.apiKey;
+    .then(function(inbox) {
+      var inboxPostCommitsUri = getLink(inbox, 'add-commit') + '?apiKey=' + instance.apiKey;
       console.log('Created inbox: ' + inbox.inboxId);
-      console.log('add-commit: ' + inbox._links['add-commit'].href);
+      console.log('add-commit: ' + getLink(inbox, 'add-commit'));
+      console.log(sep);
       return rp(post(inboxPostCommitsUri, instance.inboxCommits, 
         {
         'x-github-event': 'push'
         })
       );
     })
-    .then(function(inboxCommitsAddBody) {
-      var message = JSON.parse(inboxCommitsAddBody);
+    .then(function(message) {
       console.log('Commits added: ' + message.message);
+      message.message.should.equal('Your push event has been queued to be added to CommitStream.');
+      console.log(sep);      
       return rp(get('/' + instance.instanceId + '/query?workitem=S-11111&apiKey=' + instance.apiKey));
     })
-    .then(function(queryBody) {
-      var commitMessage = JSON.parse(queryBody).commits[0].message;
-      commitMessage.should.equal(instance.inboxCommits.commits[0].message);      
+    .then(function(queryResponse) {
+      var firstMessage = queryResponse.commits[0].message;
+      firstMessage.should.equal(instance.inboxCommits.commits[0].message); 
       console.log("JUST ONE COMMIT:");
-      console.log(commitMessage);
+      console.log(firstMessage);
       console.log("\n");
     })
     .catch(console.error)
@@ -194,11 +190,11 @@ describe('/api/instances', function() {
   }    
 
   it('Has isolated data for instance1.', function(done) {    
-    instanceTest(instance1, done);
+    instanceTest(instances[0], done);
   });
 
   it('Has isolated data for instance2.', function(done) {    
-    instanceTest(instance2, done);
+    instanceTest(instances[1], done);
   });
 
 });
