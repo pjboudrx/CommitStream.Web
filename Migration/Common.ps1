154cf86fe355ab0b3027fc5feab0be51d1859061
kunzimariano <kunzi.mariano@gmail.com>
2015-06-17 17:41:35 -0300

    S-53125 Forgot to add Common.ps1

---
+++ Migration/Common.ps1
@@ -0,0 +1,10 @@
+
+function getAuthorizationHeader {
+  param($urs, $password)
+  'Basic ' + [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($usr+":"+$password))
+}
+
+function readJson {
+  param([Parameter(ValueFromPipeline=$true)]$file)
+  Get-Content -Path $file -Raw -Encoding UTF8 | ConvertFrom-Json
+}
