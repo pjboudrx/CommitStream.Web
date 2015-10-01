b6060151f0857fdd08a62b4912c0d70a3bb7ad7b
matiasheffel <matiasheffel@hotmail.com>
2015-10-01 18:05:44 -0300

    S-54958 wip

--- src/app/middleware/csError.js
+++ src/app/middleware/csError.js
@@ -1,7 +1,43 @@
 (function() {
   var _ = require('underscore'),
     util = require('util'),
-    createCustomError = require('custom-error-generator');
+    createCustomError = require('custom-error-generator'),
+    bunyan = require('bunyan'),
+    azBunyan = require('az-bunyan');
+
+  // define the target azure storage table name
+  var tableName = 'developingapplogtable';
+
+  // define the connection string to your azure storage account
+  var connectionString = 'DefaultEndpointsProtocol=https;AccountName=commitstreamdev;AccountKey=PBr7JHysuTvIXJwljstuPLmBoVCao/UQvPVqiJQRrfXgAdXAw41hQpXKz1f+fSzQ3niJVMwgTU7fsSA+1esmIA==';
+
+  // initialize the az-bunyan table storage stream
+  var azureStreamInfo = azBunyan.createTableStorageStream('info', {
+    connectionString: connectionString,
+    tableName: tableName
+  });
+  var azureStreamError = azBunyan.createTableStorageStream('error', {
+    connectionString: connectionString,
+    tableName: tableName
+  });
+
+  var logger = bunyan.createLogger({
+    name: "CSError-Logger", // logger name
+    serializers: {
+      req: bunyan.stdSerializers.req, // standard bunyan req serializer
+      err: bunyan.stdSerializers.err // standard bunyan error serializer
+    },
+    streams: [{
+        level: 'info', // loging level
+        stream: process.stdout // log INFO and above to stdout
+      }, {
+        level: 'error', // loging level
+        stream: process.stderr // log INFO and above to stdout
+      },
+      azureStreamInfo,
+      azureStreamError
+    ]
+  });
 
   var UNEXPECTED_ERROR_MSG = 'There was an unexpected error when processing your request.';
 
@@ -34,17 +70,31 @@
   };
 
   csError.errorHandler = function(err, req, res, next) {
-    console.error("\nEXCEPTION RAISED BY API ROUTE: " + util.inspect(req.route, {
+    // logger.info("bunyan logger" + util.inspect(req.route, {
+    //   showHidden: true,
+    //   depth: null
+    // }).substr(0, 5000));
+    logger.info("bunyan logger" + req);
+    var errorMessage = "\nEXCEPTION RAISED BY API ROUTE: " + util.inspect(req.route, {
       showHidden: true,
       depth: null
-    }).substr(0, 5000));
-    console.error("STACK TRACE:");
-    console.error(err.stack);
-    console.error("CAUGHT ERROR DETAILS:");
-    console.error(util.inspect(err, {
+    }).substr(0, 5000)
+    + "\nURL: " + util.inspect(req.originalUrl)
+    + "\nHEADERS:\n" + util.inspect(req.headers, {
       showHidden: true,
       depth: null
-    }).substr(0, 5000));
+    })
+    + "\nBODY:\n" + util.inspect(req.body, {
+      showHidden: true,
+      depth: null
+    })
+    + "\nSTACK TRACE:\n"
+    + err.stack
+    + "\nCAUGHT ERROR DETAILS:\n"
+    + util.inspect(err, {
+      showHidden: true,
+      depth: null
+    }).substr(0, 5000);
 
     function sendError(error) {
       res.status(error.statusCode).json(error.errors);
@@ -53,11 +103,12 @@
     if (err instanceof CSError) {
       sendError(err);
       if (err.internalMessage !== null) {
-        console.error("INTERNAL MESSAGE:");
-        console.error(err.internalMessage);
+        errorMessage += "\nINTERNAL MESSAGE:\n" + err.internalMessage;
       }
+      logger.error(errorMessage);
     } else {
       sendError(csError([UNEXPECTED_ERROR_MSG], 500));
+      logger.error(errorMessage);
     }
   };
 
