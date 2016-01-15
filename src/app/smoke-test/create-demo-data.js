0e181d4d063739b19c0abe4df5b99f0cddad161a
kunzimariano <kunzi.mariano@gmail.com>
2016-01-15 17:46:10 -0300

    S-56379 added a --sample option for creating commits that are related to assets that exist in the demo data from v1.

--- src/app/smoke-test/create-demo-data.js
+++ src/app/smoke-test/create-demo-data.js
@@ -2,11 +2,13 @@
 
 var _regeneratorRuntime = require('babel-runtime/regenerator')['default'];
 
+var _Map = require('babel-runtime/core-js/map')['default'];
+
 var _getIterator = require('babel-runtime/core-js/get-iterator')['default'];
 
 var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
 
-var _this = this;
+var _this3 = this;
 
 var _commander = require('commander');
 
@@ -16,7 +18,7 @@ var _libCsApiClient = require('./lib/cs-api-client');
 
 var _libCsApiClient2 = _interopRequireDefault(_libCsApiClient);
 
-_commander2['default'].version('0.0.0').option('-u, --url [url]', 'The base URL for the CommitStream Service API, default: http://localhost:6565/api', 'http://localhost:6565/api').option('-i, --instances [number]', 'Number of instances to create, default: 1', 1).option('-r, --repos [number]', 'Number of repos creation iterations to run (creates one repo per family type during each iteration), default 1', 1).option('-m, --mentions [number]', 'Number of times to post a commit with each mention (one story, 5 tasks, 5 tests in each group of workitems), default 1', 1).option('-d, --debug', 'Show results of each commit, not just summary information').option('-j, --json', 'Log only the JSON output with all the query URLs needed for the performance client').parse(process.argv);
+_commander2['default'].version('0.0.0').option('-u, --url [url]', 'The base URL for the CommitStream Service API, default: http://localhost:6565/api', 'http://localhost:6565/api').option('-i, --instances [number]', 'Number of instances to create, default: 1', 1).option('-r, --repos [number]', 'Number of repos creation iterations to run (creates one repo per family type during each iteration), default 1', 1).option('-m, --mentions [number]', 'Number of times to post a commit with each mention (one story, 5 tasks, 5 tests in each group of workitems), default 1', 1).option('-d, --debug', 'Show results of each commit, not just summary information').option('-j, --json', 'Log only the JSON output with all the query URLs needed for the performance client').option('-s, --sample', 'Create the commits with sample data that exists in the PR builds', 0).parse(process.argv);
 
 var number_of_instances = parseInt(_commander2['default'].instances);
 var number_of_repo_iterations = parseInt(_commander2['default'].repos);
@@ -26,12 +28,108 @@ var client = new _libCsApiClient2['default'](_commander2['default'].url);
 
 if (!_commander2['default'].json) console.log('Operating against this CommitStream Service API: ' + client.baseUrl);
 
-var workItemsToMention = [['S-00001', 'T-00001', 'T-00002', 'T-00003', 'T-00004', 'T-00005', 'AT-00001', 'AT-00002', 'AT-00003', 'AT-00004', 'AT-00005'], ['S-00002', 'T-00011', 'T-00012', 'T-00013', 'T-00014', 'T-00015', 'AT-00011', 'AT-00012', 'AT-00013', 'AT-00014', 'AT-00015'], ['S-00003', 'T-00021', 'T-00022', 'T-00023', 'T-00024', 'T-00025', 'AT-00021', 'AT-00022', 'AT-00023', 'AT-00024', 'AT-00025'], ['S-00004', 'T-00031', 'T-00032', 'T-00033', 'T-00034', 'T-00035', 'AT-00031', 'AT-00032', 'AT-00033', 'AT-00034', 'AT-00035']];
+var createInstanceWithSampleData = function createInstanceWithSampleData(iteration) {
+  var realMentions, instance, digest, inbox;
+  return _regeneratorRuntime.async(function createInstanceWithSampleData$(context$1$0) {
+    var _this2 = this;
 
-var createInstanceWithData = function createInstanceWithData(iteration) {
-  var inboxesToCreate, instance, digest, n, inboxNum, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, inboxToCreate, inbox, workItemGroupNum, workItemsGroup, comma, _iteratorNormalCompletion2, _didIteratorError2, _iteratorError2, _iterator2, _step2, workItem, mentionNum, message, commitAddResponse;
+    while (1) switch (context$1$0.prev = context$1$0.next) {
+      case 0:
+        console.log('Creating instance wiht sample data');
 
-  return _regeneratorRuntime.async(function createInstanceWithData$(context$1$0) {
+        realMentions = new _Map();
+
+        realMentions.set('S-01041', ['AT-01075', 'AT-01076', 'AT-01077', 'AT-01085', 'TK-01078', 'TK-01079', 'TK-01080', 'TK-01098', 'TK-01100']);
+        realMentions.set('S-01042', ['AT-01078', 'AT-01079', 'AT-01080', 'AT-01081', 'AT-01082', 'TK-01081', 'TK-01082', 'TK-01083', 'TK-01084']);
+        realMentions.set('S-01043', ['AT-01083', 'AT-01084', 'AT-01086', 'AT-01087', 'TK-01086', 'TK-01087', 'TK-01088', 'TK-01089']);
+        realMentions.set('S-01064', ['AT-01097', 'TK-01113', 'TK-01114']);
+
+        context$1$0.next = 8;
+        return _regeneratorRuntime.awrap(client.instanceCreate());
+
+      case 8:
+        instance = context$1$0.sent;
+        context$1$0.next = 11;
+        return _regeneratorRuntime.awrap(instance.digestCreate({
+          description: 'Digest for ' + iteration
+        }));
+
+      case 11:
+        digest = context$1$0.sent;
+
+        if (!_commander2['default'].json) {
+          console.log('The digest: ' + digest._links['teamroom-view'].href + '&apiKey=' + client.apiKey);
+          console.log('#' + iteration + ': Populating instance ' + client.instanceId + ' (apiKey = ' + client.apiKey + ')');
+        }
+
+        context$1$0.next = 15;
+        return _regeneratorRuntime.awrap(digest.inboxCreate({
+          name: 'GitHub Repo ' + iteration,
+          family: 'GitHub'
+        }));
+
+      case 15:
+        inbox = context$1$0.sent;
+
+        realMentions.forEach(function callee$1$0(v, k, map) {
+          var message, commitAddResponse;
+          return _regeneratorRuntime.async(function callee$1$0$(context$2$0) {
+            var _this = this;
+
+            while (1) switch (context$2$0.prev = context$2$0.next) {
+              case 0:
+                message = k + ' in  ' + inbox.inboxId + ' of family = ' + inbox.family;
+                context$2$0.next = 3;
+                return _regeneratorRuntime.awrap(inbox.commitCreate(message));
+
+              case 3:
+                commitAddResponse = context$2$0.sent;
+
+                if (_commander2['default'].debug) {
+                  console.log(commitAddResponse.message);
+                }
+
+                v.forEach(function callee$2$0(v, i, array) {
+                  var message, commitAddResponse;
+                  return _regeneratorRuntime.async(function callee$2$0$(context$3$0) {
+                    while (1) switch (context$3$0.prev = context$3$0.next) {
+                      case 0:
+                        message = k + ' ' + v + ' in  ' + inbox.inboxId + ' of family = ' + inbox.family;
+                        context$3$0.next = 3;
+                        return _regeneratorRuntime.awrap(inbox.commitCreate(message));
+
+                      case 3:
+                        commitAddResponse = context$3$0.sent;
+
+                        if (_commander2['default'].debug) {
+                          console.log(commitAddResponse.message);
+                        }
+
+                      case 5:
+                      case 'end':
+                        return context$3$0.stop();
+                    }
+                  }, null, _this);
+                });
+
+              case 6:
+              case 'end':
+                return context$2$0.stop();
+            }
+          }, null, _this2);
+        });
+
+      case 17:
+      case 'end':
+        return context$1$0.stop();
+    }
+  }, null, _this3);
+};
+
+var createInstanceWithFakeData = function createInstanceWithFakeData(iteration) {
+  var inboxesToCreate, workItemsToMention, instance, digest, n, inboxNum, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, inboxToCreate, inbox, workItemGroupNum, workItemsGroup, comma, _iteratorNormalCompletion2, _didIteratorError2, _iteratorError2, _iterator2, _step2, workItem, mentionNum, message, commitAddResponse;
+
+  return _regeneratorRuntime.async(function createInstanceWithFakeData$(context$1$0) {
     while (1) switch (context$1$0.prev = context$1$0.next) {
       case 0:
         inboxesToCreate = [{
@@ -47,15 +145,18 @@ var createInstanceWithData = function createInstanceWithData(iteration) {
           name: 'VsoGit Repo ' + iteration,
           family: 'VsoGit'
         }];
-        context$1$0.next = 3;
+        workItemsToMention = [['S-00001', 'T-00001', 'T-00002', 'T-00003', 'T-00004', 'T-00005', 'AT-00001', 'AT-00002', 'AT-00003', 'AT-00004', 'AT-00005'], ['S-00002', 'T-00011', 'T-00012', 'T-00013', 'T-00014', 'T-00015', 'AT-00011', 'AT-00012', 'AT-00013', 'AT-00014', 'AT-00015'], ['S-00003', 'T-00021', 'T-00022', 'T-00023', 'T-00024', 'T-00025', 'AT-00021', 'AT-00022', 'AT-00023', 'AT-00024', 'AT-00025'], ['S-00004', 'T-00031', 'T-00032', 'T-00033', 'T-00034', 'T-00035', 'AT-00031', 'AT-00032', 'AT-00033', 'AT-00034', 'AT-00035']];
+        context$1$0.next = 4;
         return _regeneratorRuntime.awrap(client.instanceCreate());
 
-      case 3:
+      case 4:
         instance = context$1$0.sent;
-        context$1$0.next = 6;
-        return _regeneratorRuntime.awrap(instance.digestCreate({ description: 'Digest for ' + iteration }));
+        context$1$0.next = 7;
+        return _regeneratorRuntime.awrap(instance.digestCreate({
+          description: 'Digest for ' + iteration
+        }));
 
-      case 6:
+      case 7:
         digest = context$1$0.sent;
 
         if (!_commander2['default'].json) {
@@ -65,9 +166,9 @@ var createInstanceWithData = function createInstanceWithData(iteration) {
 
         n = 0;
 
-      case 9:
+      case 10:
         if (!(n < number_of_repo_iterations)) {
-          context$1$0.next = 80;
+          context$1$0.next = 81;
           break;
         }
 
@@ -75,20 +176,20 @@ var createInstanceWithData = function createInstanceWithData(iteration) {
         _iteratorNormalCompletion = true;
         _didIteratorError = false;
         _iteratorError = undefined;
-        context$1$0.prev = 14;
+        context$1$0.prev = 15;
         _iterator = _getIterator(inboxesToCreate);
 
-      case 16:
+      case 17:
         if (_iteratorNormalCompletion = (_step = _iterator.next()).done) {
-          context$1$0.next = 63;
+          context$1$0.next = 64;
           break;
         }
 
         inboxToCreate = _step.value;
-        context$1$0.next = 20;
+        context$1$0.next = 21;
         return _regeneratorRuntime.awrap(digest.inboxCreate(inboxToCreate));
 
-      case 20:
+      case 21:
         inbox = context$1$0.sent;
         workItemGroupNum = inboxNum % 4;
         workItemsGroup = workItemsToMention[workItemGroupNum];
@@ -102,128 +203,128 @@ var createInstanceWithData = function createInstanceWithData(iteration) {
         _iteratorNormalCompletion2 = true;
         _didIteratorError2 = false;
         _iteratorError2 = undefined;
-        context$1$0.prev = 29;
+        context$1$0.prev = 30;
         _iterator2 = _getIterator(workItemsGroup);
 
-      case 31:
+      case 32:
         if (_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done) {
-          context$1$0.next = 46;
+          context$1$0.next = 47;
           break;
         }
 
         workItem = _step2.value;
         mentionNum = 0;
 
-      case 34:
+      case 35:
         if (!(mentionNum < number_of_mentions_per_workitem_per_repo)) {
-          context$1$0.next = 43;
+          context$1$0.next = 44;
           break;
         }
 
         message = workItem + ' mention # ' + mentionNum + ' on ' + iteration + ' in  ' + inbox.inboxId + ' of family = ' + inbox.family;
-        context$1$0.next = 38;
+        context$1$0.next = 39;
         return _regeneratorRuntime.awrap(inbox.commitCreate(message));
 
-      case 38:
+      case 39:
         commitAddResponse = context$1$0.sent;
 
         if (_commander2['default'].debug) {
           console.log(commitAddResponse.message);
         }
 
-      case 40:
+      case 41:
         mentionNum++;
-        context$1$0.next = 34;
+        context$1$0.next = 35;
         break;
 
-      case 43:
+      case 44:
         _iteratorNormalCompletion2 = true;
-        context$1$0.next = 31;
+        context$1$0.next = 32;
         break;
 
-      case 46:
-        context$1$0.next = 52;
+      case 47:
+        context$1$0.next = 53;
         break;
 
-      case 48:
-        context$1$0.prev = 48;
-        context$1$0.t0 = context$1$0['catch'](29);
+      case 49:
+        context$1$0.prev = 49;
+        context$1$0.t0 = context$1$0['catch'](30);
         _didIteratorError2 = true;
         _iteratorError2 = context$1$0.t0;
 
-      case 52:
-        context$1$0.prev = 52;
+      case 53:
         context$1$0.prev = 53;
+        context$1$0.prev = 54;
 
         if (!_iteratorNormalCompletion2 && _iterator2['return']) {
           _iterator2['return']();
         }
 
-      case 55:
-        context$1$0.prev = 55;
+      case 56:
+        context$1$0.prev = 56;
 
         if (!_didIteratorError2) {
-          context$1$0.next = 58;
+          context$1$0.next = 59;
           break;
         }
 
         throw _iteratorError2;
 
-      case 58:
-        return context$1$0.finish(55);
-
       case 59:
-        return context$1$0.finish(52);
+        return context$1$0.finish(56);
 
       case 60:
+        return context$1$0.finish(53);
+
+      case 61:
         _iteratorNormalCompletion = true;
-        context$1$0.next = 16;
+        context$1$0.next = 17;
         break;
 
-      case 63:
-        context$1$0.next = 69;
+      case 64:
+        context$1$0.next = 70;
         break;
 
-      case 65:
-        context$1$0.prev = 65;
-        context$1$0.t1 = context$1$0['catch'](14);
+      case 66:
+        context$1$0.prev = 66;
+        context$1$0.t1 = context$1$0['catch'](15);
         _didIteratorError = true;
         _iteratorError = context$1$0.t1;
 
-      case 69:
-        context$1$0.prev = 69;
+      case 70:
         context$1$0.prev = 70;
+        context$1$0.prev = 71;
 
         if (!_iteratorNormalCompletion && _iterator['return']) {
           _iterator['return']();
         }
 
-      case 72:
-        context$1$0.prev = 72;
+      case 73:
+        context$1$0.prev = 73;
 
         if (!_didIteratorError) {
-          context$1$0.next = 75;
+          context$1$0.next = 76;
           break;
         }
 
         throw _iteratorError;
 
-      case 75:
-        return context$1$0.finish(72);
-
       case 76:
-        return context$1$0.finish(69);
+        return context$1$0.finish(73);
 
       case 77:
+        return context$1$0.finish(70);
+
+      case 78:
         n++;
-        context$1$0.next = 9;
+        context$1$0.next = 10;
         break;
 
-      case 80:
+      case 81:
       case 'end':
         return context$1$0.stop();
     }
-  }, null, _this, [[14, 65, 69, 77], [29, 48, 52, 60], [53,, 55, 59], [70,, 72, 76]]);
+  }, null, _this3, [[15, 66, 70, 78], [30, 49, 53, 61], [54,, 56, 60], [71,, 73, 77]]);
 };
 
 var run = function run() {
@@ -232,31 +333,46 @@ var run = function run() {
     while (1) switch (context$1$0.prev = context$1$0.next) {
       case 0:
         if (_commander2['default'].json) console.log('[');
-        instanceNum = 0;
 
-      case 2:
-        if (!(instanceNum < number_of_instances)) {
-          context$1$0.next = 8;
+        if (!_commander2['default'].sample) {
+          context$1$0.next = 6;
           break;
         }
 
-        context$1$0.next = 5;
-        return _regeneratorRuntime.awrap(createInstanceWithData(instanceNum));
+        context$1$0.next = 4;
+        return _regeneratorRuntime.awrap(createInstanceWithSampleData(new Date().toGMTString()));
 
-      case 5:
-        instanceNum++;
-        context$1$0.next = 2;
+      case 4:
+        context$1$0.next = 13;
         break;
 
-      case 8:
+      case 6:
+        instanceNum = 0;
+
+      case 7:
+        if (!(instanceNum < number_of_instances)) {
+          context$1$0.next = 13;
+          break;
+        }
+
+        context$1$0.next = 10;
+        return _regeneratorRuntime.awrap(createInstanceWithFakeData(instanceNum));
+
+      case 10:
+        instanceNum++;
+        context$1$0.next = 7;
+        break;
+
+      case 13:
         if (_commander2['default'].json) console.log(']');
 
-      case 9:
+      case 14:
       case 'end':
         return context$1$0.stop();
     }
-  }, null, _this);
+  }, null, _this3);
 };
+
 try {
   run();
 } catch (e) {
--- src/app/smoke-test/es6/create-demo-data.js
+++ src/app/smoke-test/es6/create-demo-data.js
@@ -1,3 +1,5 @@
+'use strict';
+
 import program from 'commander';
 import CSApiClient from './lib/cs-api-client';
 
@@ -9,6 +11,7 @@ program
   .option('-m, --mentions [number]', 'Number of times to post a commit with each mention (one story, 5 tasks, 5 tests in each group of workitems), default 1', 1)
   .option('-d, --debug', 'Show results of each commit, not just summary information')
   .option('-j, --json', 'Log only the JSON output with all the query URLs needed for the performance client')
+  .option('-s, --sample', 'Create the commits with sample data that exists in the PR builds', 0)
   .parse(process.argv);
 
 const number_of_instances = parseInt(program.instances);
@@ -19,35 +22,77 @@ let client = new CSApiClient(program.url);
 
 if (!program.json) console.log(`Operating against this CommitStream Service API: ${client.baseUrl}`);
 
-let workItemsToMention = [
-  ['S-00001', 'T-00001', 'T-00002', 'T-00003', 'T-00004', 'T-00005', 'AT-00001', 'AT-00002', 'AT-00003', 'AT-00004', 'AT-00005'],
-  ['S-00002', 'T-00011', 'T-00012', 'T-00013', 'T-00014', 'T-00015', 'AT-00011', 'AT-00012', 'AT-00013', 'AT-00014', 'AT-00015'],
-  ['S-00003', 'T-00021', 'T-00022', 'T-00023', 'T-00024', 'T-00025', 'AT-00021', 'AT-00022', 'AT-00023', 'AT-00024', 'AT-00025'],
-  ['S-00004', 'T-00031', 'T-00032', 'T-00033', 'T-00034', 'T-00035', 'AT-00031', 'AT-00032', 'AT-00033', 'AT-00034', 'AT-00035']  
-];
+let createInstanceWithSampleData = async(iteration) => {
+  console.log('Creating instance wiht sample data');
 
-let createInstanceWithData = async (iteration) => {
-  let inboxesToCreate = [
-    {
-      name: `GitHub Repo ${iteration}`,
-      family: 'GitHub'
-    },
-    {
-      name: `GitLab Repo ${iteration}`,
-      family: 'GitLab'
-    },
-    {
-      name: `Bitbucket Repo ${iteration}`,
-      family: 'Bitbucket'
-    },
-    {
-      name: `VsoGit Repo ${iteration}`,
-      family: 'VsoGit'
+  let realMentions = new Map();
+  realMentions.set('S-01041', ['AT-01075', 'AT-01076', 'AT-01077', 'AT-01085', 'TK-01078', 'TK-01079', 'TK-01080', 'TK-01098', 'TK-01100']);
+  realMentions.set('S-01042', ['AT-01078', 'AT-01079', 'AT-01080', 'AT-01081', 'AT-01082', 'TK-01081', 'TK-01082', 'TK-01083', 'TK-01084']);
+  realMentions.set('S-01043', ['AT-01083', 'AT-01084', 'AT-01086', 'AT-01087', 'TK-01086', 'TK-01087', 'TK-01088', 'TK-01089']);
+  realMentions.set('S-01064', ['AT-01097', 'TK-01113', 'TK-01114']);
+
+  let instance = await client.instanceCreate();
+  let digest = await instance.digestCreate({
+    description: `Digest for ${iteration}`
+  });
+
+  if (!program.json) {
+    console.log(`The digest: ${digest._links['teamroom-view'].href}&apiKey=${client.apiKey}`);
+    console.log(`#${iteration}: Populating instance ${client.instanceId} (apiKey = ${client.apiKey})`);
+  }
+
+  let inbox = await digest.inboxCreate({
+    name: `GitHub Repo ${iteration}`,
+    family: 'GitHub'
+  });
+
+  realMentions.forEach(async(v, k, map) => {
+    let message = `${k} in  ${inbox.inboxId} of family = ${inbox.family}`;
+    let commitAddResponse = await inbox.commitCreate(message);
+
+    if (program.debug) {
+      console.log(commitAddResponse.message);
     }
+
+    v.forEach(async(v, i, array) => {
+      let message = `${k} ${v} in  ${inbox.inboxId} of family = ${inbox.family}`;
+      let commitAddResponse = await inbox.commitCreate(message);
+
+      if (program.debug) {
+        console.log(commitAddResponse.message);
+      }
+
+    });
+
+  });
+};
+
+let createInstanceWithFakeData = async(iteration) => {
+  let inboxesToCreate = [{
+    name: `GitHub Repo ${iteration}`,
+    family: 'GitHub'
+  }, {
+    name: `GitLab Repo ${iteration}`,
+    family: 'GitLab'
+  }, {
+    name: `Bitbucket Repo ${iteration}`,
+    family: 'Bitbucket'
+  }, {
+    name: `VsoGit Repo ${iteration}`,
+    family: 'VsoGit'
+  }];
+
+  let workItemsToMention = [
+    ['S-00001', 'T-00001', 'T-00002', 'T-00003', 'T-00004', 'T-00005', 'AT-00001', 'AT-00002', 'AT-00003', 'AT-00004', 'AT-00005'],
+    ['S-00002', 'T-00011', 'T-00012', 'T-00013', 'T-00014', 'T-00015', 'AT-00011', 'AT-00012', 'AT-00013', 'AT-00014', 'AT-00015'],
+    ['S-00003', 'T-00021', 'T-00022', 'T-00023', 'T-00024', 'T-00025', 'AT-00021', 'AT-00022', 'AT-00023', 'AT-00024', 'AT-00025'],
+    ['S-00004', 'T-00031', 'T-00032', 'T-00033', 'T-00034', 'T-00035', 'AT-00031', 'AT-00032', 'AT-00033', 'AT-00034', 'AT-00035']
   ];
 
   let instance = await client.instanceCreate();
-  let digest = await instance.digestCreate({description:`Digest for ${iteration}`});
+  let digest = await instance.digestCreate({
+    description: `Digest for ${iteration}`
+  });
 
   if (!program.json) {
     console.log(`The digest: ${digest._links['teamroom-view'].href}&apiKey=${client.apiKey}`);
@@ -65,9 +110,8 @@ let createInstanceWithData = async (iteration) => {
       if (!program.json) {
         console.log(`Adding commits to ${inbox.inboxId} of family ${inbox.family}`);
         console.log(`${inbox._links['add-commit'].href}?apiKey=${client.apiKey}`);
-      }
-      else console.log(`${comma}"${client.baseUrl}/${client.instanceId}/commits/tags/versionone/workitem?numbers=${workItemsGroup.join(',')}&apiKey=${client.apiKey}"`);
-      for(let workItem of workItemsGroup) {
+      } else console.log(`${comma}"${client.baseUrl}/${client.instanceId}/commits/tags/versionone/workitem?numbers=${workItemsGroup.join(',')}&apiKey=${client.apiKey}"`);
+      for (let workItem of workItemsGroup) {
         for (let mentionNum = 0; mentionNum < number_of_mentions_per_workitem_per_repo; mentionNum++) {
           let message = `${workItem} mention # ${mentionNum} on ${iteration} in  ${inbox.inboxId} of family = ${inbox.family}`;
           let commitAddResponse = await inbox.commitCreate(message);
@@ -78,15 +122,20 @@ let createInstanceWithData = async (iteration) => {
       }
     }
   }
-};
+}
 
-let run = async () => {
+let run = async() => {
   if (program.json) console.log('[');
-  for(let instanceNum = 0; instanceNum < number_of_instances; instanceNum++) {
-    await createInstanceWithData(instanceNum);
+  if (program.sample) {
+    await createInstanceWithSampleData((new Date()).toGMTString());
+  } else {
+    for (let instanceNum = 0; instanceNum < number_of_instances; instanceNum++) {
+      await createInstanceWithFakeData(instanceNum);
+    }
   }
   if (program.json) console.log(']');
 }
+
 try {
   run();
 } catch (e) {
